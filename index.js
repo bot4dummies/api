cf7be7c106d571e7265be0ea4e0bdace9cd2f577
Julian Pardo <julipardodiaz@Julians-MacBook-Pro.local>
2018-06-30 12:59:16 -0500

    Modify booljs sample

--- index.js
+++ index.js
@@ -5,12 +5,8 @@ const Bool = require('booljs');
 // Here is where magic happens
 module.exports = (async () => {
     try {
-        return new Bool('{{namespace}}', [
-            {{{dependencies}}}
-        ])
-            .setServerDrivers({{{options.serverDrivers}}})
-            .setDatabaseDrivers({{{options.databaseDrivers}}})
-            .run();
+        return new Bool('com.bot4dummies.api')
+          .run();
     } catch (error) {
         console.error(error);
     }
